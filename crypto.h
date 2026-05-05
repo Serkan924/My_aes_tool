cd5a339f51533c9e2f05d2ceb8631faa36c3074f
Serkan924 <serkandilber12@gmail.com>
2026-05-05 19:30:58 +0300

    Add encryption and decryption function declarations

--- crypto.h
+++ crypto.h
@@ -1,1 +1,12 @@
+#ifndef CRYPTO_H
+#define CRYPTO_H
 
+int encrypt(unsigned char *plaintext, int plaintext_len,
+            unsigned char *key, unsigned char *iv,
+            unsigned char *ciphertext);
+
+int decrypt(unsigned char *ciphertext, int ciphertext_len,
+            unsigned char *key, unsigned char *iv,
+            unsigned char *plaintext);
+
+#endif

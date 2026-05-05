934c3678e121ca5761de74076512d2b7ae92316b
Serkan924 <serkandilber12@gmail.com>
2026-05-05 19:31:27 +0300

    Update crypto.c

--- crypto.c
+++ crypto.c
@@ -1,1 +1,46 @@
+#include <openssl/evp.h>
+#include "crypto.h"
 
+int encrypt(unsigned char *plaintext, int plaintext_len,
+            unsigned char *key, unsigned char *iv,
+            unsigned char *ciphertext) {
+
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    int len, ciphertext_len;
+
+    EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv);
+
+    EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len);
+    ciphertext_len = len;
+
+    EVP_EncryptFinal_ex(ctx, ciphertext + len, &len);
+    ciphertext_len += len;
+
+    EVP_CIPHER_CTX_free(ctx);
+
+    return ciphertext_len;
+}
+
+int decrypt(unsigned char *ciphertext, int ciphertext_len,
+            unsigned char *key, unsigned char *iv,
+            unsigned char *plaintext) {
+
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    int len, plaintext_len;
+
+    EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv);
+
+    EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len);
+    plaintext_len = len;
+
+    if (EVP_DecryptFinal_ex(ctx, plaintext + len, &len) <= 0) {
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;
+    }
+
+    plaintext_len += len;
+
+    EVP_CIPHER_CTX_free(ctx);
+
+    return plaintext_len;
+}
